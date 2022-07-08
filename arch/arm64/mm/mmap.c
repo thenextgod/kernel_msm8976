1a1e6685faeedf24b674ff34399a5a90154e9906
Abhijith Ravikumar <70852403+thenextgod@users.noreply.github.com>
2022-07-08 13:09:51 +0200

    Update mmap.c

--- arch/arm64/mm/mmap.c
+++ arch/arm64/mm/mmap.c
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #include <linux/elf.h>
 #include <linux/fs.h>
 #include <linux/mm.h>
@@ -26,49 +25,37 @@
 #include <linux/io.h>
 #include <linux/personality.h>
 #include <linux/random.h>
-
 #include <asm/cputype.h>
-
 /*
  * Leave enough space between the mmap area and the stack to honour ulimit in
  * the face of randomisation.
  */
 #define MIN_GAP (SZ_128M + ((STACK_RND_MASK << PAGE_SHIFT) + 1))
 #define MAX_GAP	(STACK_TOP/6*5)
-
 static int mmap_is_legacy(void)
 {
 	if (current->personality & ADDR_COMPAT_LAYOUT)
 		return 1;
-
 	if (rlimit(RLIMIT_STACK) == RLIM_INFINITY)
 		return 1;
-
 	return sysctl_legacy_va_layout;
 }
-
 static unsigned long mmap_rnd(void)
 {
 	unsigned long rnd = 0;
-
 	if (current->flags & PF_RANDOMIZE)
 		rnd = (long)get_random_int() & STACK_RND_MASK;
-
 	return rnd << PAGE_SHIFT;
 }
-
 static unsigned long mmap_base(void)
 {
 	unsigned long gap = rlimit(RLIMIT_STACK);
-
 	if (gap < MIN_GAP)
 		gap = MIN_GAP;
 	else if (gap > MAX_GAP)
 		gap = MAX_GAP;
-
 	return PAGE_ALIGN(STACK_TOP - gap - mmap_rnd());
 }
-
 /*
  * This function, called very early during the creation of a new process VM
  * image, sets up which VM layout function to use:
@@ -90,8 +77,6 @@ void arch_pick_mmap_layout(struct mm_struct *mm)
 	}
 }
 EXPORT_SYMBOL_GPL(arch_pick_mmap_layout);
-
-
 /*
  * You really shouldn't be using read() or write() on /dev/mem.  This might go
  * away in the future.
@@ -102,10 +87,8 @@ int valid_phys_addr_range(unsigned long addr, size_t size)
 		return 0;
 	if (addr + size > __pa(high_memory - 1) + 1)
 		return 0;
-
 	return 1;
 }
-
 /*
  * Do not allow /dev/mem mappings beyond the supported physical range.
  */
@@ -113,11 +96,8 @@ int valid_mmap_phys_addr_range(unsigned long pfn, size_t size)
 {
 	return !(((pfn << PAGE_SHIFT) + size) & ~PHYS_MASK);
 }
-
 #ifdef CONFIG_STRICT_DEVMEM
-
 #include <linux/ioport.h>
-
 /*
  * devmem_is_allowed() checks to see if /dev/mem access to a certain address
  * is valid. The argument is a physical page number.  We mimic x86 here by
@@ -132,5 +112,4 @@ int devmem_is_allowed(unsigned long pfn)
 		return 1;
 	return 0;
 }
-
 #endif
